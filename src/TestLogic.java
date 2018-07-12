18fd0ef388eaa97d982b1da92b9b74909ed1f3b9
i338008 <rashmi.r02@sap.com>
2018-07-12 13:37:36 +0530

    added testFindCube testcase

--- src/TestLogic.java
+++ src/TestLogic.java
@@ -10,4 +10,10 @@ public class TestLogic {
         assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
         assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
     }  
+    
+    @Test  
+    public void testFindCube(){  
+        assertEquals(8,Calculation.findCube(2);  
+        assertEquals(27,Calculation.findCube(3);  
+    }
 }
